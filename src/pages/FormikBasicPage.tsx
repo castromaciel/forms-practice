f209a34f9d83b06aa661b7f9f676fd3f5adc55c2
castromaciel <castromaciel00@gmail.com>
2023-07-17 23:03:12 -0300

    manual validations

--- src/pages/FormikBasicPage.tsx
+++ src/pages/FormikBasicPage.tsx
@@ -1,10 +1,40 @@
-import { useFormik } from 'formik'
+import { FormikErrors, useFormik } from 'formik'
+
+interface FormValues {
+  firstName: string
+  lastName: string
+  email: string
+}
 
 const FormikBasicPage = () => {
+  const validate = ({ email, firstName, lastName }: FormValues) => {
+    const errors:FormikErrors<FormValues> = {}
+    
+    if (!firstName) {
+      errors.firstName = 'Required'
+    } else if (firstName.length >= 15) {
+      errors.firstName = 'Must be 15 characters or less'
+    }
+
+    if (!lastName) {
+      errors.lastName = 'Required'
+    } else if (lastName.length >= 12) {
+      errors.lastName = 'Must be 12 characters or less'
+    }
+
+    if (!email) {
+      errors.email = 'Required'
+    } else if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+      errors.email = 'Invalid email address'
+    }
+
+    return errors
+  }
+
   const {
     handleChange, values: {
       email, firstName, lastName
-    }, handleSubmit
+    }, handleSubmit, errors, touched, handleBlur
   } = useFormik({
     initialValues: {
       firstName: '',
@@ -13,7 +43,9 @@ const FormikBasicPage = () => {
     },
     onSubmit: (values) => {
       console.log(values)
-    }
+    },
+    validate,
+    validateOnBlur: true
   })
 
   return (
@@ -28,10 +60,11 @@ const FormikBasicPage = () => {
             name="firstName"
             id="firstName" 
             onChange={handleChange}
+            onBlur={handleBlur}
             value={firstName}
           />
         </label>
-        <span>First name is required</span>
+        {touched.firstName && errors?.firstName && <span>{errors.firstName}</span>}
         
         <label htmlFor="lastName">
           Last name
@@ -40,10 +73,11 @@ const FormikBasicPage = () => {
             name="lastName"
             id="lastName"
             onChange={handleChange}
+            onBlur={handleBlur}
             value={lastName}
           />
         </label>
-        <span>Last name is required</span>
+        {touched.lastName && errors?.lastName && <span>{errors.lastName}</span>}
         
         <label htmlFor="email">
           Email address
@@ -52,11 +86,11 @@ const FormikBasicPage = () => {
             name="email"
             id="email" 
             onChange={handleChange}
+            onBlur={handleBlur}
             value={email}
           />
         </label>
-        <span>Email is required</span>
-        <span>Check for an valid email format</span>
+        {touched.email && errors?.email && <span>{errors.email}</span>}
 
         <button type="submit">Enviar</button>
       </form>
